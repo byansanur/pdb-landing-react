36bfa4aa49656166c792eefbc3ca62caa1ccbb14
byansanur <ratbyansa25@gmail.com>
2020-11-21 15:26:43 +0700

    first landing

---
+++ src/components/header.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+export class Header extends Component {
+  render() {
+    return (
+      <header id="header">
+        <div className="intro">
+          <div className="overlay">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-8 col-md-offset-2 intro-text">
+                  <h1>
+                    {this.props.data ? this.props.data.title : "Loading"}
+                    <span></span>
+                  </h1>
+                  <p>
+                    {this.props.data ? this.props.data.paragraph : "Loading"}
+                  </p>
+                  <a
+                    href="https://api.whatsapp.com/send?phone=+6285722220561&text=halo saya ingin tanya tentang layanan yang ada"
+                    className="btn btn-custom btn-lg page-scroll"
+                  >
+                    Hubungi Kami
+                  </a>{" "}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
